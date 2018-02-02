54f658b56d9c1a1fe23c9b382163f0e5979fcadf
justinwkUKM <waqas@siswa.ukm.edu.my>
2018-02-02 17:10:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/waqas/quizapp/ResultActivity.java
@@ -0,0 +1,40 @@
+package com.example.waqas.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView scoreTxtView = (TextView) findViewById(R.id.score);
+        RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar1);
+        ImageView img = (ImageView)findViewById(R.id.img1);
+
+
+        Bundle b = getIntent().getExtras();
+        int score = b.getInt("score");
+        ratingBar.setRating(score);
+        scoreTxtView.setText(String.valueOf(score));
+
+        if(score == 0){
+            img.setImageResource(R.drawable.score0);
+        }else if(score == 1){
+            img.setImageResource(R.drawable.score1);
+        }else if(score == 2){
+            img.setImageResource(R.drawable.score2);
+        }else if(score == 3){
+            img.setImageResource(R.drawable.score3);
+        }else if(score == 4){
+            img.setImageResource(R.drawable.score4);
+        }else if(score == 5){
+            img.setImageResource(R.drawable.score5);
+        }
+    }
+}
